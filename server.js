373622c3dd1c847fee0a69056b143b4cb7a3b40f
tejamedida <tejam@thoughtworks.com>
2019-02-05 13:04:37 +0530

    [Teja] server responds with helloworld for /

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = new express();
 let PORT = 3000;
-app.listen(PORT,()=> console.log(`server listening at port ${PORT}`));
+app.listen(PORT, () => console.log(`server listening at port ${PORT}`));
 
+app.get('/', (req,res) => res.send("Hello world"));

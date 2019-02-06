b46746e5939889cb96bc6167e25ad31b096dfdc5
tejamedida <tejam@thoughtworks.com>
2019-02-06 08:47:39 +0530

    [Teja] Responding with hardcodeed todos on /todos

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = new express();
 let PORT = 3000;
+
+const fetchTodos = function(){
+    return "{1:{title:hardcoded,discription:testing}}";
+}
 app.listen(PORT, () => console.log(`server listening at port ${PORT}`));
 
-app.get('/', (req,res) => res.send("Hello world"));
+app.get('/', (req, res) => res.send("Hello world"));
+
+app.get('/todos', (req,res) => {
+    let todos = fetchTodos();
+    res.send(todos);
+})
